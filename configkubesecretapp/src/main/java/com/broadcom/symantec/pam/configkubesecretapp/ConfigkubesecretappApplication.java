12e41e3ce4b0b061e4cc34cbc0fea5d970874026
Joe Burke <jb657067@broadcom.net>
2020-08-26 11:00:59 -0400

    service #2

---
+++ configkubesecretapp/src/main/java/com/broadcom/symantec/pam/configkubesecretapp/ConfigkubesecretappApplication.java
@@ -0,0 +1,13 @@
+package com.broadcom.symantec.pam.configkubesecretapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigkubesecretappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigkubesecretappApplication.class, args);
+	}
+
+}
---
+++ configkubesecretapp/src/test/java/com/broadcom/symantec/pam/configfileapp/ConfigfileappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broadcom.symantec.pam.configfileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigfileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
