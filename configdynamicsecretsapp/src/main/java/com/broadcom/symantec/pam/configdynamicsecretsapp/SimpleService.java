e698a355a8e1100090654182b6d2e394e2096030
Joe Burke <jb657067@broadcom.net>
2020-08-26 23:18:36 -0400

    changes to call a2a

--- configdynamicsecretsapp/src/main/java/com/broadcom/symantec/pam/configdynamicsecretsapp/SimpleService.java
+++ configdynamicsecretsapp/src/main/java/com/broadcom/symantec/pam/configdynamicsecretsapp/SimpleService.java
@@ -1,8 +1,15 @@
 package com.broadcom.symantec.pam.configdynamicsecretsapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient.Version;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -13,11 +20,29 @@ import java.util.Date;
 @CrossOrigin
 public class SimpleService {
 
+    @Value("${ALIAS}")
+    String alias;
+    
+    private static final String U_START = "<U>";
+    private static final String U_END = "</U>";
+
+    private static final String P_START = "<P>";
+    private static final String P_END = "</P>";
+
     @GetMapping("/")
     public String home () throws Exception {
- 
-		String user ="";
-		String pw ="";
+        
+        String uri = "http://localhost:28090/requestScript/retrieveCredentials?aliasName="+ alias + "&contentType=xml";
+
+        HttpClient client = HttpClient.newBuilder().version(Version.HTTP_1_1).build();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(uri)).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String responseBody = response.body();
+
+		String user =responseBody.substring(responseBody.indexOf(U_START),responseBody.indexOf(U_END));
+		String pw   =responseBody.substring(responseBody.indexOf(P_START),responseBody.indexOf(P_END));
+     
+        System.out.println("u="+user+"  p="+pw);
 
 		//make A2A call
 
@@ -31,7 +56,6 @@ public class SimpleService {
             // Setup the connection with the DB
             connect = DriverManager
                     .getConnection("jdbc:mysql://192.168.72.132:3306/demo",user,pw);
-            //        .getConnection("jdbc:mysql://192.168.72.132:3306/demo","user1","MyPassword");
             // Statements allow to issue SQL queries to the database
             statement = connect.createStatement();
             // Result set get the result of the SQL query
@@ -47,10 +71,11 @@ public class SimpleService {
                 int id = resultSet.getInt("id");
                 Date date = resultSet.getDate("created_at");
 
-                sb.append(name + " is a " + desc + " with employee id = "+ id + " hired on : " + date + "\n");
+                sb.append("Dynamic Secrets   > " + name + " is a " + desc + " with employee id = "+ id + " hired on : " + date + "\n");
             }
         } catch (Exception e) {
             e.printStackTrace();
+            sb.append("Dynamic Secrets   > " + e.getMessage() + "\n");
         } finally {
             if (resultSet != null) {
                 resultSet.close();
